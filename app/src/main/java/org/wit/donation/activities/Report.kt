631fd93206a4e05d1e9f543180a4ef58cb75e38c
Ben Capper <Bencapper19@gmail.com>
2022-01-23 20:36:38 +0000

    Add menu files
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

---
+++ app/src/main/java/org/wit/donation/activities/Report.kt
@@ -0,0 +1,12 @@
+package org.wit.donation.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.wit.donation.R
+
+class Report : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
