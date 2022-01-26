b28ac1ae4437c8c1f0e450ef2255b8bd47ad6390
Ben Capper <Bencapper19@gmail.com>
2022-01-26 17:34:41 +0000

    Create NavHostFragment
    Add destinations to the nav graph
    Donate fragment loads into the NavHostFragment
    
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

---
+++ app/src/main/java/org/wit/donation/activities/Home.kt
@@ -0,0 +1,12 @@
+package org.wit.donation.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.wit.donation.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+}
